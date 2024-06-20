c28cc96d8a11157d4f1bddf09288db6617dfba63
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-06-20 23:33:53 +0300

    Updating channel function

--- src/commands/update_announce_channel.ts
+++ src/commands/update_announce_channel.ts
@@ -1,39 +1,32 @@
 import {
-    ChatInputCommandInteraction,
-    SlashCommandBuilder,
-    SlashCommandChannelOption,
-    SlashCommandNumberOption,
-    SlashCommandUserOption,
-  } from "discord.js";
-  import { ExtendedClient } from "../interfaces/ExtendedClient";
-  import { isAdmin } from "../utils/isAdmin";
-  import { addBalance } from "../models/User";
-  import {
-    updateAdminChannel,
-    updateMessageRewardRates,
-  } from "../models/Rewards";
-  
-  export const data = new SlashCommandBuilder()
-    .setName("update_announce_channel")
-    .setDescription("Update Admin")
-    .addChannelOption(
-      new SlashCommandChannelOption()
-        .setName("announce_channel")
-        .setDescription("New Channel")
-        .setRequired(true)
-    );
-  
-  export async function execute(
-    bot: ExtendedClient,
-    interaction: ChatInputCommandInteraction
-  ) {
-    if (await isAdmin(interaction)) {
-      await interaction.deferReply();
-      const channel = interaction.options.getChannel("announce_channel");
-      await updateAdminChannel(channel.id);
-      return await interaction.followUp({
-        content: `Announcement Channel Has been updated :D`,
-      });
-    }
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  SlashCommandChannelOption,
+} from "discord.js";
+import { ExtendedClient } from "../interfaces/ExtendedClient";
+import { isAdmin } from "../utils/isAdmin";
+import { updadeAnnouncementChannel } from "../models/Rewards";
+
+export const data = new SlashCommandBuilder()
+  .setName("update_announce_channel")
+  .setDescription("Update Admin")
+  .addChannelOption(
+    new SlashCommandChannelOption()
+      .setName("announce_channel")
+      .setDescription("New Channel")
+      .setRequired(true)
+  );
+
+export async function execute(
+  bot: ExtendedClient,
+  interaction: ChatInputCommandInteraction
+) {
+  if (await isAdmin(interaction)) {
+    await interaction.deferReply();
+    const channel = interaction.options.getChannel("announce_channel");
+    await updadeAnnouncementChannel(channel.id);
+    return await interaction.followUp({
+      content: `Announcement Channel Has been updated :D`,
+    });
   }
-  
+}
