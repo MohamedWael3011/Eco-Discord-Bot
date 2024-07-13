f0819c0b9096c92b3dd3b62d7b40a58bf694557c
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-07-13 17:47:35 +0300

    Adjusting opensea calls

--- src/utils/opensea/opensea.ts
+++ src/utils/opensea/opensea.ts
@@ -2,21 +2,31 @@ import sdk from "@api/opensea";
 import { config } from "dotenv";
 import { errorHandler } from "../../logger/errorHandler";
 config();
-async function getListings(tokenId: number, bot) {
+
+const MAX_RETRIES = 3; // Maximum number of retries
+const RETRY_DELAY = 2000; // Delay between retries in milliseconds
+
+async function getListings(tokenId: number, bot, retries = MAX_RETRIES) {
   try {
     sdk.auth(process.env.OPENSEA);
     sdk.server("https://api.opensea.io");
-    const lisitngs = await sdk.get_listings({
+    const listings = await sdk.get_listings({
       asset_contract_address: process.env.CONTRACT,
       token_ids: tokenId,
       chain: "matic",
       protocol: "seaport",
     });
-    console.log(lisitngs.data);
-
-    return lisitngs.data;
+    console.log(listings.data);
+    return listings.data;
   } catch (err) {
-    errorHandler(bot, err, "Fetching Listings");
+    if (retries > 0) {
+      console.log(`Retrying... (${MAX_RETRIES - retries + 1}/${MAX_RETRIES})`);
+      await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY)); // Wait for the delay
+      return getListings(tokenId, bot, retries - 1); // Retry the function
+    } else {
+      errorHandler(bot, err, "Fetching Listings");
+      throw err; // Rethrow the error after exhausting retries
+    }
   }
 }
 
@@ -25,14 +35,18 @@ function getTimestampInSeconds() {
 }
 
 export default async function isListed(tokenId: number, bot) {
-  const listings = await getListings(tokenId, bot);
-  let flag = false;
-  const currentTimestamp = getTimestampInSeconds();
-  listings.orders.forEach((order) => {
-    if (order.expiration_time >= currentTimestamp) {
-      flag = true;
-    }
-  });
-
-  return flag;
+  try {
+    const listings = await getListings(tokenId, bot);
+    let flag = false;
+    const currentTimestamp = getTimestampInSeconds();
+    listings.orders.forEach((order) => {
+      if (order.expiration_time >= currentTimestamp) {
+        flag = true;
+      }
+    });
+    return flag;
+  } catch (err) {
+    errorHandler(bot, err, "Checking if listing");
+    return false;
+  }
 }
